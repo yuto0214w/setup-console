1f159cc523e68ea04a1a2f1ebc92053bcd24045e
yuto0214w <yuto0214w@gmail.com>
2023-08-06 19:28:52 +0900

    feat: implement try_init()

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # setup-console
-A library that provides a function to fix ANSI escaping problem in Windows.
+A library that provides a function to fix ANSI escaping problem in Command Prompt on Windows.
 
 [![Latest version](https://img.shields.io/crates/v/setup-console.svg)](https://crates.io/crates/setup-console)
 
 ## Usage
-Simply run `setup_console::init()` in the main function before printing anything.
+Simply run `setup_console::init()` (or `setup_console::try_init()`) in the main function before printing anything.
 
 ```rust
 fn main() {
@@ -18,10 +18,11 @@ fn main() {
 
 ## Panics
 **This only applies to Windows.**
-This function doesn't panic, but shows message and terminates program with error code obtained from
+`setup_console::init()` doesn't panic, but shows message and terminates program with error code obtained from
 [`GetLastError()`](https://learn.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror)
 instead.
 The situations that would lead program to terminate would be
 - when stdout is invalid (e.g. `program 1>nul`)
 - when program fails to run [`GetConsoleMode()`](https://learn.microsoft.com/en-us/windows/console/getconsolemode)
 - when program fails to run [`SetConsoleMode()`](https://learn.microsoft.com/en-us/windows/console/setconsolemode)
+If this is not your desired behavior, you can use `setup_console::try_init()` which returns `Result`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,72 @@
+#[derive(Debug, Clone)]
+pub enum SetupConsoleErrorType {
+    InvalidOutputHandle,
+    GetConsoleModeFailed,
+    SetConsoleModeFailed,
+}
+
+#[derive(Debug, Clone)]
+pub struct SetupConsoleError(SetupConsoleErrorType);
+
+impl std::fmt::Display for SetupConsoleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self.0 {
+                SetupConsoleErrorType::InvalidOutputHandle => "stdout is invalid",
+                SetupConsoleErrorType::GetConsoleModeFailed => "failed to get console mode",
+                SetupConsoleErrorType::SetConsoleModeFailed => "failed to set console mode",
+            }
+        )
+    }
+}
+
 #[cfg(target_os = "windows")]
-pub fn init() {
+pub fn try_init() -> Result<(), SetupConsoleError> {
     use windows_sys::Win32::{
-        Foundation::{GetLastError, INVALID_HANDLE_VALUE},
-        System::{
-            Console::{
-                GetConsoleMode, GetStdHandle, SetConsoleMode, ENABLE_VIRTUAL_TERMINAL_PROCESSING,
-                STD_OUTPUT_HANDLE,
-            },
-            Threading::ExitProcess,
+        Foundation::INVALID_HANDLE_VALUE,
+        System::Console::{
+            GetConsoleMode, GetStdHandle, SetConsoleMode, ENABLE_VIRTUAL_TERMINAL_PROCESSING,
+            STD_OUTPUT_HANDLE,
         },
     };
     unsafe {
-        let exit_with_error = |message: &str| {
-            eprintln!("Error: {}", message);
-            ExitProcess(GetLastError());
-        };
         let std_output = GetStdHandle(STD_OUTPUT_HANDLE);
         if std_output == INVALID_HANDLE_VALUE {
-            exit_with_error("stdout is invalid");
+            return Err(SetupConsoleError(
+                SetupConsoleErrorType::InvalidOutputHandle,
+            ));
         }
         let mut console_mode = 0;
         if GetConsoleMode(std_output, &mut console_mode) == 0 {
-            exit_with_error("failed to get console mode");
+            return Err(SetupConsoleError(
+                SetupConsoleErrorType::GetConsoleModeFailed,
+            ));
         }
         console_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
         if SetConsoleMode(std_output, console_mode) == 0 {
-            exit_with_error("failed to set console mode");
+            return Err(SetupConsoleError(
+                SetupConsoleErrorType::SetConsoleModeFailed,
+            ));
         }
+    }
+    Ok(())
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn try_init() -> Result<(), SetupConsoleError> {
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+pub fn init() {
+    use windows_sys::Win32::{Foundation::GetLastError, System::Threading::ExitProcess};
+    if let Err(error) = try_init() {
+        eprintln!("Error: {}", error);
+        unsafe {
+            ExitProcess(GetLastError());
+        };
     }
 }
 
